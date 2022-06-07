2d622fdea9dfea3aea8bad8494218372aa15e9e3
Денис Сергеев <D-D1983@yandex.ru>
2022-06-07 15:27:16 +0400

    test modified

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -1,6 +1,7 @@
 package ru.netology;
 
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.*;
@@ -10,9 +11,13 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class CardOrderTest {
 
+    @BeforeEach
+    void Setup() {
+        open("http://localhost:9999");
+    }
+
     @Test
     void shouldSendValidForm() {
-        open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Иван Иванов");
         $("[data-test-id=phone] input").setValue("+79000000000");
         $("[data-test-id=agreement]").click();
@@ -22,8 +27,8 @@ public class CardOrderTest {
 
     @Test
     void shouldSendInvalidName() {
-        open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Ivan Ivanov");
+        $("[data-test-id=phone] input").setValue("+79000000000");
         $("[data-test-id=agreement]").click();
         $(".button").click();
         $(".input_type_text .input__sub").shouldHave(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
@@ -31,8 +36,8 @@ public class CardOrderTest {
 
     @Test
     void shouldSendEmptyName() {
-        open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("");
+        $("[data-test-id=phone] input").setValue("+79000000000");
         $("[data-test-id=agreement]").click();
         $(".button").click();
         $(".input_type_text .input__sub").shouldHave(text("Поле обязательно для заполнения"));
@@ -40,7 +45,6 @@ public class CardOrderTest {
 
     @Test
     void shouldSendInvalidNumber() {
-        open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Иван Иванов");
         $("[data-test-id=phone] input").setValue("+799999999999");
         $("[data-test-id=agreement]").click();
@@ -50,7 +54,6 @@ public class CardOrderTest {
 
     @Test
     void shouldSendEmptyNumber() {
-        open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Иван Иванов");
         $("[data-test-id=phone] input").setValue("");
         $("[data-test-id=agreement]").click();
@@ -60,7 +63,6 @@ public class CardOrderTest {
 
     @Test
     void shouldInvalidCheckbox() {
-        open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Иван Иванов");
         $("[data-test-id=phone] input").setValue("+79000000000");
         $(".button").click();
